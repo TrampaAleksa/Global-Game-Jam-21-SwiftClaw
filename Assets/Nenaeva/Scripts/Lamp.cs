47210d824721f780968adc7bb8721c7238c30646
Vladan_Trampa <trampavlad@gmail.com>
2021-01-27 22:44:00 +0100

    Adding light object with luminocity script

---
+++ Assets/Nenaeva/Scripts/Lamp.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+    public Light lightToFade;
+    public float eachFadeTime = 2f;
+    public float fadeWaitTime = 5f;
+    IEnumerator fadeInAndOut(Light lightToFade, bool fadeIn, float duration)
+    {
+        float minLuminosity = 0; // min intensity
+        float maxLuminosity = 20; // max intensity
+
+        float counter = 0f;
+
+        //Set Values depending on if fadeIn or fadeOut
+        float a, b;
+
+        if (fadeIn)
+        {
+            a = minLuminosity;
+            b = maxLuminosity;
+        }
+        else
+        {
+            a = maxLuminosity;
+            b = minLuminosity;
+        }
+
+        float currentIntensity = lightToFade.intensity;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+
+            lightToFade.intensity = Mathf.Lerp(a, b, counter / duration);
+
+            yield return null;
+        }
+    }
+    //Fade in and out forever
+    IEnumerator fadeInAndOutRepeat(Light lightToFade, float duration, float waitTime)
+    {
+        WaitForSeconds waitForXSec = new WaitForSeconds(waitTime);
+
+        while (true)
+        {
+            //Fade out
+            yield return fadeInAndOut(lightToFade, false, duration);
+
+            //Wait
+            yield return waitForXSec;
+
+            //Fade-in 
+            yield return fadeInAndOut(lightToFade, true, duration);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(fadeInAndOutRepeat(lightToFade, eachFadeTime, fadeWaitTime));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
+
