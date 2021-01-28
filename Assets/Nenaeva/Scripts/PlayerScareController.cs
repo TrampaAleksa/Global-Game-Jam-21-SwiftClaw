ecc23814605cec28d756761424486ab143882da9
Aleksa Trampa <alektramp@gmail.com>
2021-01-28 19:52:35 +0100

    Moved project resources into folders. Added Scare Filed script

---
+++ Assets/Nenaeva/Scripts/PlayerScareController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+internal class PlayerScareController : MonoBehaviour
+{
+    public void RunToPoint(Transform runToPoint)
+    {
+        print("Player is scared, running!");
+    }
+}
---
+++ Assets/Nenaeva/Scripts/ScareField.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ScareField : MonoBehaviour
+{
+    public Transform[] runToPoints;
+
+    private void ScarePlayer(PlayerScareController scareController)
+    {
+        scareController.RunToPoint(runToPoints[Random.Range(0, runToPoints.Length)]);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ScarePlayer(other.GetComponent<PlayerScareController>());
+        }
+    }
+}
