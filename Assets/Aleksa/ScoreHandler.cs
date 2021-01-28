c8ced19c5427586d021bd8c344f83cbf27a5fb8f
Aleksa Trampa <alektramp@gmail.com>
2021-01-28 17:54:20 +0100

    won the game condition

--- Assets/Aleksa/ScoreHandler.cs
+++ Assets/Aleksa/ScoreHandler.cs
@@ -3,10 +3,16 @@
 public class ScoreHandler : MonoBehaviour
 {
     private static int NumberOfPickedUp;
+    public int scoreForWin;
 
     public void IncreaseScore()
     {
         NumberOfPickedUp++;
         print("Score increased : " + NumberOfPickedUp);
+
+        if (NumberOfPickedUp >= scoreForWin)
+        {
+            print("Won the game!");
+        }
     }
 }
