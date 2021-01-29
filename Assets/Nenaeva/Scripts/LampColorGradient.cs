8fda20ebc40536be37565338554200a41de2375f
Aleksa Trampa <alektramp@gmail.com>
2021-01-30 00:37:50 +0100

    color gradient when near boat part

--- Assets/Nenaeva/Scripts/LampColorGradient.cs
+++ Assets/Nenaeva/Scripts/LampColorGradient.cs
@@ -13,35 +13,47 @@ public class LampColorGradient : MonoBehaviour
 
     private void Awake()
     {
-        timedAction = gameObject.AddComponent<TimedAction>();
+        timedAction = gameObject.AddComponent<TimedAction>().DestroyOnFinish(false);
         timedAction.AddTickAction(GradualColorChange);
     }
 
     private void Start()
     {
         lightHandler = GameManager.Instance.lightHandler;
-        farColor = lightHandler.lamp.lightToFade.color;
-        targetColor = nearColor;
+        
+        var color = lightHandler.lamp.lightToFade.color;
+        farColor = new Color(color.r, color.g , color.b, color.a);
+        targetColor = new Color(nearColor.r, nearColor.g, nearColor.b, nearColor.a);
     }
 
     public void LampIsNear()
     {
-        targetColor = nearColor;
+        timedAction.CancelTimer();
+        targetColor = new Color(nearColor.r, nearColor.g, nearColor.b, nearColor.a);
         counter = 0f;
+        
+        var color = lightHandler.lamp.lightToFade.color;
+        colorOnStart = new Color(color.r, color.g , color.b, color.a);
+        
         timedAction.StartTimedAction(() => print("near ship part"), gradientSpeed);
     }
 
     public void LampIsFar()
     {
-        targetColor = farColor;
+        timedAction.CancelTimer();
+        targetColor = new Color(farColor.r, farColor.g, farColor.b, farColor.a);;
         counter = 0f;
+        
+        var color = lightHandler.lamp.lightToFade.color;
+        colorOnStart = new Color(color.r, color.g , color.b, color.a);
         timedAction.StartTimedAction(() => print("far from ship part"), gradientSpeed);
     }
 
     private float counter;
+    private Color colorOnStart;
     private void GradualColorChange()
     {
         counter += Time.deltaTime;
-        lightHandler.lamp.lightToFade.color = Color.Lerp(lightHandler.lamp.lightToFade.color, targetColor, counter/ gradientSpeed);
+        lightHandler.lamp.lightToFade.color = Color.Lerp(colorOnStart, targetColor, counter/ gradientSpeed);
     }
 }
