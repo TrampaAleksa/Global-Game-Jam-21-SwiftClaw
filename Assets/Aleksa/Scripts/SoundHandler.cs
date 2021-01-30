8bca1470c447dc0d43c0c96bf4fb6fbd2b6368b0
Aleksa Trampa <alektramp@gmail.com>
2021-01-30 16:09:13 +0100

    extracted sound handler to contain every sound

---
+++ Assets/Aleksa/Scripts/SoundHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class SoundHandler : MonoBehaviour
+{
+    public AudioSource snakeWarning;
+    public AudioSource boatRepair;
+    public AudioSource pickupPart;
+    public AudioSource snakeHiss;
+    public AudioSource scream;
+}
