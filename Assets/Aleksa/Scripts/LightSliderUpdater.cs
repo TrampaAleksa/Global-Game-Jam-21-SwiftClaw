3db1d219036e5e41be7e28c990f3fa529b797665
Aleksa Trampa <alektramp@gmail.com>
2021-01-30 13:12:40 +0100

    Added slider updater

---
+++ Assets/Aleksa/Scripts/LightSliderUpdater.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LightSliderUpdater : MonoBehaviour
+{
+    private Slider slider;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    private void Start()
+    {
+        slider.maxValue = GameManager.Instance.lightHandler.lamp.maxLightIntensity;
+    }
+
+    private void Update()
+    {
+        slider.value = GameManager.Instance.lightHandler.lamp.currentLightValue;
+    }
+}
