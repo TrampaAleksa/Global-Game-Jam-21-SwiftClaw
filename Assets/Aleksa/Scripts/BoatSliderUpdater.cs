56511dc85a0c030aab1708f34afd2b24f98ad805
Aleksa Trampa <alektramp@gmail.com>
2021-01-30 13:55:49 +0100

    boat slider update

---
+++ Assets/Aleksa/Scripts/BoatSliderUpdater.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BoatSliderUpdater : MonoBehaviour
+{
+    private Slider slider;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    private void Start()
+    {
+        slider.maxValue = GameManager.Instance.scoreHandler.scoreForWin;
+    }
+
+    private void Update()
+    {
+        slider.value = GameManager.Instance.scoreHandler.TotalPartsDelivered;
+    }
+}
