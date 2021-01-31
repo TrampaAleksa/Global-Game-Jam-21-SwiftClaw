5ee8dd9450863d1355ab9e787570dffacf4fa039
Aleksa Trampa <alektramp@gmail.com>
2021-01-31 10:23:03 +0100

    Added prefabs and models for island. Added water and rain to main scene

---
+++ Assets/Laki/UI/Scripts/FollowObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform toFollow;
+
+    public void Update()
+    {
+        var transformOfObject = transform;
+        transformOfObject.position = new Vector3(toFollow.position.x, transformOfObject.position.y, toFollow.position.z);
+    }
+}
