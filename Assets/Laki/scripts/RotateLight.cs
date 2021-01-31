e439f29f36c9bdfc821eaae7a09fcfb27e04f525
Lazar Andric <unLa4ky@users.noreply.github.com>
2021-01-31 15:28:00 +0100

    Add rotation script

---
+++ Assets/Laki/scripts/RotateLight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour
+{
+    public Vector3 axis;
+    public float add;
+    public float time;
+    private void Update()
+    {
+        gameObject.LeanRotateAroundLocal(axis, add, time);
+    }
+}
