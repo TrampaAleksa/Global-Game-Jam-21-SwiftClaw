6ba0359730f42c98c733ec6ba3befd4b8630a153
pajocar23 <56831069+pajocar23@users.noreply.github.com>
2021-01-31 11:08:35 +0100

    Lighting
    sound+vfx (light missing)

---
+++ Assets/Najpametniji programer ikada/Scenes/aseti za okolinu/scripts/ThunderSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderSpawner : MonoBehaviour
+{
+    public GameObject[] lightings;
+    public Transform[] spawnPoints;
+    private Transform nextCheckPoint;
+
+    public int spawnSecondsMin = 0;
+    public int spawnSecondsMax = 10;
+
+    float spawnTime;
+    public float soundTime=1f;
+    public float destroyThunderTime = 0.9f;
+    public AudioSource thunderSound;
+
+
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator StartSpawning()
+    {
+        spawnTime = Random.Range(spawnSecondsMin, spawnSecondsMax);
+
+        yield return new WaitForSeconds(spawnTime);       
+        GameObject lighting=Instantiate(lightings[Random.Range(0, lightings.Length)],spawnPoints[Random.Range(0, spawnPoints.Length)].position, Random.rotation);
+
+        yield return new WaitForSeconds(soundTime);
+        thunderSound.Play();
+
+        yield return new WaitForSeconds(destroyThunderTime);
+        Destroy(lighting);
+
+
+
+        StartCoroutine(StartSpawning());
+    }
+
+}
